67dfad9febc55dfbc2f43f1fda20a5e2e27671f0
Esuyawkal Bereda <esuyawkal07@gmail.com>
2025-12-26 18:21:22 -0500

    commit seven --- fixed darkmode issues with profile and settings icons

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,27 @@
 
 import Image from "next/image";
 
-import {useState} from 'react';
+import { useEffect, useState } from "react";
+
 
 export default function Home() {
-    const [isDark, setIsDark] = useState(() => {
-      if(typeof window !== 'undefined'){
-      const saved = localStorage.getItem('darkMode');
-      if(saved === 'true') {
-        document.documentElement.classList.add('dark');
-      return false}}
-    });
-  
+  const [isDark, setIsDark] = useState(() => {
+    if (typeof window === "undefined") return false;
+    return localStorage.getItem("darkMode") === "true";
+  });
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark", isDark);
+    localStorage.setItem("darkMode", String(isDark));
+  }, [isDark]);
+
     const toggleDarkMode = () => {
       const newValue = !isDark;
       setIsDark(newValue);
       localStorage.setItem('darkMode', String(newValue));
       if (newValue) {
         document.documentElement.classList.add("dark");
+        
       } 
       else {
         document.documentElement.classList.remove("dark");
@@ -43,8 +47,9 @@ export default function Home() {
           </div>
           <button className="right-0">
             <Image
-              className="invert-with-dark"
-              src="/profile.svg"
+            id="profile"
+              className=""
+              src={isDark ? "/profile_white.svg" : "/profile.svg"}
               alt="profile.js logo"
               width={40}
               height={40}
@@ -60,7 +65,7 @@ export default function Home() {
             Automatically summarize meetings, extract tasks, and schedule your next steps with AI.
           </p>
         </div>
-        <div className="flex flex-col md:gap-20 text-base font-medium sm:flex-row sm:gap-5 mb-40 mx-auto">
+        <div className="flex flex-col md:gap-20 text-base font-medium sm:flex-row sm:gap-5 py-[8vh] mx-auto">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background border border-black/8 transition-colors hover:text-white hover:bg-[#383838] dark:border-white/8 dark:hover:bg-[#ccc] dark:hover:text-black md:w-[180px]"
             href="!"
@@ -85,8 +90,9 @@ export default function Home() {
             Contact</a>
              <button onClick={toggleDarkMode}>
               <Image
-                className="invert-with-dark"
-                src="/settings.svg"
+              id="settings"
+                className=""
+                src={isDark ? "/settings_white.svg" : "/settings.svg"}
                 alt="settings.js logo"
                 width={40}
                 height={40}
